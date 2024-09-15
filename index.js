b882f9b4837e8f35d22ee244ca37006393b50f03
rahul1905-sk <rahul1905.sk@gmail.com>
2024-09-16 03:36:29 +0530

    cd

--- index.js
+++ index.js
@@ -48,11 +48,9 @@ const UserData = mongoose.model("UserData", UserSchema);
 
 // POST route to store user data
 app.get("/", async (req, res) => {
-  try {
+  
     res.status(200).json({ message: "API is working!" });
-  } catch (error) {
-    res.status(500).json({ error: "Failed to save data." });
-  }
+  
 });
 
 
